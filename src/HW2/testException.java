e3e8780e6ab3628e9c5a5ebd519b58b2be888892
DmitriyDmitrishin <dmitrishin52@gmail.com>
2019-10-10 20:38:45 +0300

    Lesson 2 HW 2. Все выполнил. Прошу проверить. Спасибо!

--- src/HW2/testException.java
+++ src/HW2/testException.java
@@ -8,6 +8,7 @@ public class testException {
     public static void main(String[] args) {
         int a = 4;
         int b = 4;
+
         try {
             doException(a, b);
         } catch (MyArraySizeException | MyArrayDataException e) {
@@ -16,19 +17,20 @@ public class testException {
         }
 
     }
-    private static void doException(int a, int b) throws MyArrayDataException{
+
+    private static void doException(int a, int b) throws MyArrayDataException {
 
         String[][] array = new String[a][b];
         ArrayList<Integer> list = new ArrayList<Integer>();
         int sum = 0;
 
-        if(a != 4 || b != 4){
+        if (a != 4 || b != 4) {
             throw new MyArraySizeException();
         }
         array[0][0] = "6";
-        array[0][1] = "2";
-        array[0][2] = "2";
-        array[0][3] = "6";
+        array[0][1] = "6";
+        array[0][2] = "b";
+        array[0][3] = "3";
 
         array[1][0] = "2";
         array[1][1] = "9";
@@ -45,18 +47,22 @@ public class testException {
         array[3][2] = "9";
         array[3][3] = "9";
 
+
         for (int i = 0; i < a; i++)
             for (int j = 0; j < b; j++) {
-//                System.out.println("Замени символ на число в ячейке по индексом " + "[" + i + "]" + "[" + j + "]");
-                int i1 = parseInt(array[i][j]);
-                list.add(i1);
+                int i1 = 0;
+                try {
+                    i1 = parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
                 }
+                list.add(i1);
+            }
 
-        for(int i : list) {
+        for (int i : list) {
             sum = sum + i;
         }
         System.out.println("Сумма всех чисел массива равна " + sum);
 
     }
-
 }
